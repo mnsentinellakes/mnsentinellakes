2a7c0546eaa4ea5e8e67f99097d2f6bbb40ab51f
Tim Martin <tim.martin@state.mn.us>
2019-04-05 16:30:41 -0500

    Changed function name

---
+++ R/lakename2id.R
@@ -0,0 +1,30 @@
+#' Lake Name to LakeIds
+#'
+#' This function creates a data frame with the LakeIds for all lakes with the input name, as well as the County they are located in. If multiple lakes have the
+#' same name, this will return a list of LakeIds.
+#' @param lakename name of the lake.
+#' @param county county where the lake is located. Default is NULL.
+#' @keywords lakeid dowlknum
+#' @return a data.frame
+#' @family Sentinel Lakes Tools
+#' @examples
+#' lakename2id("Echo")
+#'
+#' @export
+
+lakename2id=function(lakename,county=NULL){
+
+  lakedata=mnsentinellakes::mnlakesmetadata
+
+  if (!is.null(county)){
+    lakedata=lakedata[lakedata$County==county,]
+  }
+  lakeoutput=lakedata$LakeId[lakedata$Lake==lakename]
+
+  if (length(lakeoutput)>1)(
+    warning("There are multiple lakes with this name.")
+  )else if (length(lakeoutput)==0){
+    warning("There are no lakes with this name.")
+  }
+  return(lakeoutput)
+}
