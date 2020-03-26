341882b102375d081230e2725daa8f6b43848678
Tim Martin <Tim.Martin@state.mn.us>
2020-03-26 16:07:34 -0500

    Added fixlakeid step

--- R/wqstations.R
+++ R/wqstations.R
@@ -9,8 +9,8 @@
 
 wqstations = function(lakeid){
 
-  if (lakeid %in% unique(mnsentinellakes::sentinelmnpcastations$LakeId)){
-    stations=mnsentinellakes::sentinelmnpcastations$ID_CODE[mnsentinellakes::sentinelmnpcastations$LakeId==lakeid]
+  if (lakeid %in% mnsentinellakes::fixlakeid(unique(mnsentinellakes::sentinelmnpcastations$LakeId))){
+    stations=mnsentinellakes::sentinelmnpcastations$ID_CODE[mnsentinellakes::fixlakeid(mnsentinellakes::sentinelmnpcastations$LakeId)==lakeid]
   }else if (lakeid %in% unique(mnsentinellakes::mnpcastations$LakeId)){
     stations=mnsentinellakes::mnpcastations$ID_CODE[mnsentinellakes::mnpcastations$LakeId==lakeid & mnsentinellakes::mnpcastations$Status=="Active"]
   }else{
