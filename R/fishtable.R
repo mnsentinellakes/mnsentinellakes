19585e14b9542ba1f2748ce8dada86515300e09d
Tim Martin <Tim.Martin@state.mn.us>
2021-08-11 12:51:36 -0500

    removed erroneous lines

--- R/fishtable.R
+++ R/fishtable.R
@@ -33,8 +33,6 @@
 
 fishtable=function(fishsurvey,fishspecies,startyear=NULL,endyear=NULL){
 
-  fishsurvey=mnsentinellakes::fishsurveydata(mnsentinellakes::lakefinderdownload(mnsentinellakes::lakename2id("Carlos","Douglas")))
-  fishspecies = ""
 
   if(!is.null(fishsurvey)){
     if (!is.null(startyear)){
