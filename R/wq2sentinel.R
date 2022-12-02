26b683968e9a93e5f42cbcfc0453ce408ce2b0e2
Tim Martin <Tim.Martin@state.mn.us>
2022-12-02 12:16:33 -0600

    Added Collecting_Organization field

--- R/wq2sentinel.R
+++ R/wq2sentinel.R
@@ -13,29 +13,31 @@
 #' y <- wq2sentinel(x)
 #' @export
 
-wq2sentinel=function(wqdata){
+wq2sentinel = function(wqdata){
 
-  wqoutput=NULL
+  wqoutput = NULL
   for (i in unique(wqdata$stationName)){
 
-    wqdataselect=wqdata[wqdata$stationName==i,]
-    lakeid=mnsentinellakes::fixlakeid(unique(substr(wqdataselect$stationId,1,10)))
-    lakename=mnsentinellakes::lakeid2name(lakeid)
+    wqdataselect = wqdata[wqdata$stationName == i,]
+    lakeid = mnsentinellakes::fixlakeid(unique(substr(wqdataselect$stationId,1,10)))
+    lakename = mnsentinellakes::lakeid2name(lakeid)
 
-    wqoutputrow=data.frame("Lake"=lakename,"LakeId"=lakeid,"StationId"=wqdataselect$stationId,"Date"=as.Date(wqdataselect$sampleDate),
-                           "Time"=wqdataselect$sampleTime,"Parameter"=as.character(wqdataselect$parameter),"GTLT"=wqdataselect$gtlt,"Value"=wqdataselect$result,
-                           "Unit"=as.character(wqdataselect$resultUnit),"Fraction_Type"=as.character(wqdataselect$sampleFractionType),
-                           "Type"=as.character(wqdataselect$sampleType),"Method"=as.character(wqdataselect$testMethodName),
-                           "Upper_Depth"=suppressWarnings(as.numeric(as.character(wqdataselect$sampleUpperDepth))),
-                           "Lower_Depth"=suppressWarnings(as.numeric(as.character(wqdataselect$sampleLowerDepth))),
-                           "Comments"=as.character(wqdataselect$comments),stringsAsFactors = FALSE)
-    wqoutput=rbind(wqoutput,wqoutputrow)
+    wqoutputrow = data.frame("Lake" = lakename,"LakeId"=lakeid,"StationId" = wqdataselect$stationId,"Date" = as.Date(wqdataselect$sampleDate),
+                           "Time" = wqdataselect$sampleTime,"Parameter" = as.character(wqdataselect$parameter),"GTLT" = wqdataselect$gtlt,
+                           "Value" = wqdataselect$result,"Unit" = as.character(wqdataselect$resultUnit),
+                           "Fraction_Type" = as.character(wqdataselect$sampleFractionType),"Type" = as.character(wqdataselect$sampleType),
+                           "Method" = as.character(wqdataselect$testMethodName),
+                           "Upper_Depth" = suppressWarnings(as.numeric(as.character(wqdataselect$sampleUpperDepth))),
+                           "Lower_Depth" = suppressWarnings(as.numeric(as.character(wqdataselect$sampleLowerDepth))),
+                           "Collecting_Organization" = wqdataselect$collectingOrg,
+                           "Comments" = as.character(wqdataselect$comments),stringsAsFactors = FALSE)
+    wqoutput = rbind(wqoutput,wqoutputrow)
   }
-    wqoutput=wqoutput[order(wqoutput$Lake,wqoutput$Date),]
-    row.names(wqoutput)=NULL
-    wqoutput$GTLT[wqoutput$GTLT=="(null)"]=NA
-    wqoutput$Value[wqoutput$Value=="(null)"]=NA
-    wqoutput$Comments[wqoutput$Comments=="(null)"]=NA
+    wqoutput = wqoutput[order(wqoutput$Lake,wqoutput$Date),]
+    row.names(wqoutput) = NULL
+    wqoutput$GTLT[wqoutput$GTLT == "(null)"] = NA
+    wqoutput$Value[wqoutput$Value == "(null)"] = NA
+    wqoutput$Comments[wqoutput$Comments == "(null)"] = NA
 
   return(wqoutput)
 }
