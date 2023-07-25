2aedbb8f22585820746f7327201d81e6a90c92bf
Tim Martin <Tim.Martin@state.mn.us>
2023-07-25 16:11:53 -0500

    Rewrote to fix issues

--- R/ptcalibration.R
+++ R/ptcalibration.R
@@ -3,9 +3,8 @@
 #' This function calibrates pressure transducer data to known staff gauge or other lake level measurements.
 #' @param ptdata a data frame of pressure transducer data with "Date", "Time", and "Value" fields.
 #' @param lakeid the lakeid for the lake where the pressure transducer is located.
-#' @param staffgauge a data frame staff gauge or other lake level measurement data that the pressure transducer data are calibrated to. Includes "Lake",
+#' @param staffgaugereadings a data frame staff gauge or other lake level measurement data that the pressure transducer data are calibrated to. Includes "Lake",
 #' "Gauge_Reading", "Date", "Time", and "Notes" fields.
-#' @param notes any additional notes for this transducer and time period. If NA, the function will look for notes in the staffgauge data.
 #' @return a list of two data frames, one with the calibrated data and the other with metadata.
 #' @family Water Levels
 #' @examples
@@ -13,73 +12,102 @@
 #' x <- ptcalibration(
 #'        ptdata = data,
 #'        lakeid = "15001000",
-#'        staffgauge = staffgauge
+#'        staffgaugereadings = staffgauge
 #'      )
 #' }
 #'
 #' @export
 
-ptcalibration=function(ptdata,lakeid,staffgauge,notes=NA){
+ptcalibration = function(ptdata,lakeid,staffgaugereadings){
 
-  ptdata=as.data.frame(ptdata)
-  if(ncol(ptdata)==2){
-    ptdata["Date"]=as.Date(ptdata[,1])
-    ptdata["Time"]=as.character(ptdata[,1],format="%H:%M")
-    ptdata[,1]=NULL
-    ptdata=ptdata[,c(2,3,1)]
-  }
+  #Convert inputs to data frames
+  ptdata = as.data.frame(ptdata)
+  staffgaugereadings = as.data.frame(staffgaugereadings)
 
+  #Update pt data column names
+  colnames(ptdata) = c("date_time","value")
 
-  ptdata[,3]=as.numeric(ptdata[,3])
-  ptdata["date_time"]=as.POSIXct(paste(as.character(ptdata[,1]),as.character(ptdata[,2],format="%H:%M")), format="%Y-%m-%d %H:%M")
-  ptdata$Date=as.character(ptdata[,1])
-  ptdata$Time=as.character(ptdata[,2],format="%H:%M")
+  #get lake name from lakeid
+  lakename = mnsentinellakes::lakeid2name(lakeid)
 
-  staffgauge=staffgauge[staffgauge$Lake==mnsentinellakes::lakeid2name(lakeid),]
-  staffgauge["date_time"]=as.POSIXct(paste(as.character(staffgauge$Date),as.character(staffgauge$Time,format="%H:%M")), format="%Y-%m-%d %H:%M")
-  staffgauge$Date=as.character(staffgauge$Date)
-  staffgauge$Time=as.character(staffgauge$Time,format="%H:%M")
+  #Select Staff Gauge Readings for Lake
+  staffgaugereadings = staffgaugereadings[which(staffgaugereadings$Lake == lakename),]
 
-  if(is.na(staffgauge$Time)){
-    if(staffgauge$Date %in% ptdata[,1]){
-      print("Method 1")
-      ptdataday=ptdata[as.Date(ptdata[,1])==as.Date(staffgauge$Date),]
-      ptdatadaymean=mean(ptdataday[,3])
-      offset=ptdatadaymean-as.numeric(as.character(staffgauge$Gauge_Reading))
-      originalvalue=ptdatadaymean
+  #Remove NA Values in the pt data
+  ptdata = ptdata[which(!is.na(ptdata$value)),]
 
-      offsetdate=staffgauge$Date
-      offsettime=NA
-      offsetmethod="Specific time not recorded for staff gauge reading, calculated the offset using the difference between the mean daily pressure transducer value and the staff gauge reading for the indicated date."
-    }else{
-      stop("The date of the staff gauge reading is not in the pressure transducer data.")
+  #Check if there are any staffgaugereadings for the identified lake
+  if (nrow(staffgaugereadings) > 0){
+    #Get the min and max dates in the pt data
+    mindate = as.Date(min(ptdata$date_time))
+    maxdate = as.Date(max(ptdata$date_time))
+
+    #Determine if there are any non-NA times in the staff gauge readings and remove NAs
+    if (any(!is.na(staffgaugereadings$Time))){
+      staffgaugereadings = staffgaugereadings[which(!is.na(staffgaugereadings$Time)),]
     }
+
+    #Select staff gauge readings that only fall withing the min and max dates of the data. If there are still multiple rows, select the first.
+    staffgaugereadings = staffgaugereadings[which(staffgaugereadings$Date >= mindate & staffgaugereadings$Date <= maxdate),]
+    staffgaugereadings = staffgaugereadings[1,]
+    print(staffgaugereadings$Time)
+
+    if (!is.na(staffgaugereadings$Time)){
+      message("Using Provided Time to Calibrate Data")
+
+      #Get time zone from the pt data
+      tzid = lubridate::tz(ptdata$date_time)
+
+      #Get staffgauge date and time
+      staffgaugedate = staffgaugereadings$Date
+      staffgaugetime = as.character(format(staffgaugereadings$Time,format = "%H:%M:%S"))
+
+      #Get the date and time of the staff gauge reading
+      staffgaugedatetime = as.POSIXct(paste(staffgaugedate,staffgaugetime),
+                                      format = "%Y-%m-%d %H:%M",tz = tzid)
+
+      #Calculate the time differences between each pt data point and the staffgaugedatetime
+      ptdata$diff_time = abs(ptdata$date_time - staffgaugedatetime)
+
+      #Get calibration value (pt - sg)
+      ptvalue = ptdata$value[which(ptdata$diff_time == min(ptdata$diff_time))][1]
+      sgvalue = staffgaugereadings$Gauge_Reading
+
+      calvalue = round(ptvalue - sgvalue,2)
+      offsetmethod = "Calculated the offset using the difference between the pressure transducer value and the staff gauge reading for the indicated date and time."
+    }else{
+      message("Using Daily Mean to Calibrate Data")
+
+      staffgaugedate = staffgaugereadings$Date
+      staffgaugetime = staffgaugereadings$Time
+
+      #Calculate the doy of of the staff gauge reading
+      sfdoy = lubridate::yday(staffgaugereadings$Date)
+
+      #Calculate doy of the pt data
+      ptdata$doy = lubridate::yday(ptdata$date_time)
+
+      #Get mean pt value for sf doy
+      ptvalue = mean(ptdata[which(ptdata$doy == sfdoy),2])
+      sgvalue = staffgaugereadings$Gauge_Reading
+
+      calvalue = round(ptvalue - sgvalue)
+      offsetmethod = "Specific time not recorded for staff gauge reading, calculated the offset using the difference between the mean daily pressure transducer value
+      and the staff gauge reading for the indicated date."
+    }
+
+    dataoutput = data.frame("Lake" = lakename,"LakeId" = lakeid,"Date_Time" = ptdata$date_time,
+                            "Transducer_Value" = ptdata$value,"Adjusted_Value" = round(ptdata$value - calvalue,digits = 2))
+
+    datametadata = data.frame("Lake" = lakename,"LakeId" = lakeid,"Offset_Date" = staffgaugedate,"Offset_Time" = staffgaugetime,"Transducer" = ptvalue,
+                              "Staff_Gauge" = sgvalue,"Offset_value" = calvalue,"Offset_Method" = offsetmethod,"Notes" = staffgaugereadings$Notes)
+
+
   }else{
-    if(staffgauge$Date %in% ptdata[,1] & staffgauge$Time %in% ptdata[,2]){
-      print("Method 2")
-      ptdatatime = ptdata[which(ptdata[,4]==staffgauge$date_time),]
-      offset=ptdatatime[,3]-staffgauge$Gauge_Reading
-      ptdatadaymean=NA
-      originalvalue=ptdatatime[,3]
-      offsetdate=staffgauge$Date
-      offsettime=staffgauge$Time
-      offsetmethod="Calculated the offset using the difference between the pressure transducer value and the staff gauge reading for the indicated date and time."
-    }else{
-      stop("The date and time of the staff gauge reading is not in the pressure transducer data.")
-    }
+    message(paste0("No staff gauge readings for ",lakename))
   }
 
-  if(is.na(notes) & !is.na(staffgauge$Notes)){
-    notes=staffgauge$Notes
-  }
+  output = list(dataoutput,datametadata)
 
-    dataoutput=data.frame("Lake"=mnsentinellakes::lakeid2name(lakeid),"LakeId"=lakeid,"Date_Time"=ptdata[,4],
-                          "Transducer_Value"=ptdata[,3],"Adjusted_Value"=round(ptdata[,3]-offset,digits = 2))
-    datametadata=data.frame("Lake"=mnsentinellakes::lakeid2name(lakeid),"LakeId"=lakeid,
-                            "Offset_Date"=offsetdate,"Offset_Time"=offsettime,"Transducer"=originalvalue,"Staff_Gauge"=staffgauge$Gauge_Reading,
-                            "Offset_value"=offset,"Offset_Method"=offsetmethod,"Notes"=notes)
-
-    outputdata=list("Data"=dataoutput,"Metadata"=datametadata)
-
-  return(outputdata)
+  return(output)
 }
