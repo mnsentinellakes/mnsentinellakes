4aa1d5a7d35318bde6566c21d69dc91155af54b2
Tim Martin <Tim.Martin@state.mn.us>
2024-02-29 16:11:38 -0600

    Updated to reflect changes in the source data

--- R/icedownload.R
+++ R/icedownload.R
@@ -15,70 +15,36 @@
 
 icedownload=function(lakeid){
 
-  lakename=mnsentinellakes::mnlakesmetadata$Lake[mnsentinellakes::mnlakesmetadata$LakeId==mnsentinellakes::fixlakeid(lakeid)]
-  if (length(lakename)>0){
-  iceoutput=NULL
+  lakename = mnsentinellakes::mnlakesmetadata$Lake[mnsentinellakes::mnlakesmetadata$LakeId==mnsentinellakes::fixlakeid(lakeid)]
+  if (length(lakename) > 0){
+  iceoutput = NULL
   for (i in c("in","out")){
-    if (i=="in"){
-      icestatus="In"
-    }else if (i=="out"){
-      icestatus="Out"
-    }
-    inputurl=paste0("https://maps1.dnr.state.mn.us/cgi-bin/climatology/ice_",i,"_by_lake.cgi?id=",lakeid)
-
-    x=as.character(xml2::read_html(inputurl))
-    x=as.character(regmatches(x,gregexpr("\\[.+\\]",x)))
-    if(x=="character(0)"){
-      reshapedf=NULL
-    }else{
-      x=gsub(c("\\["),"",x)
-      x=gsub(c("\\]"),"",x)
-      x=data.frame(strsplit(x,"[},{]"))
-      colnames(x)="Row"
-      x=suppressWarnings(tidyr::separate(
-        x,
-        "Row",
-        into = c("Header","Value"),
-        sep = "[:]"))
-      x=x[!is.na(x$Value),]
-      x$Header=gsub("\"","",x$Header)
-      x$Value=gsub("\"","",x$Value)
-      reshapedf=NULL
-
-      for (k in 0:((nrow(x)/3)-1)){
-        # k=1
-        selection=(k*3)+c(1,2,3)
-        rows=x[selection,]
-
-        #Date
-        daterow=rows$Value[which(rows$Header=="date")]
-        #Source
-        sourcerow=rows$Value[which(rows$Header=="source")]
-        #Comment
-        if ("comments" %in% rows$Header){
-          commentrow=rows$Value[which(rows$Header=="comments")]
-        }else{
-          commentrow=""
-        }
-        reshaperow=data.frame("Date"=daterow,"Source"=sourcerow,"Comment"=commentrow)
-        reshapedf=rbind(reshapedf,reshaperow)
-      }
-      reshapedf["Ice_Status"]=icestatus
+    if (i == "in"){
+      icestatus = "In"
+    }else if (i == "out"){
+      icestatus = "Out"
     }
 
-    iceoutput=rbind(iceoutput,reshapedf)
+    inputurl = paste0("https://maps.dnr.state.mn.us/cgi-bin/climatology/ice_",i,"_by_lake_as_csv.cgi?id=",lakeid)
+    icedata = readr::read_csv(inputurl,skip = 2)
+
+    if(nrow(icedata) > 0){
+      colnames(icedata)[1] = "Date"
+
+      icedata = data.frame("Lake" = lakename,"LakeId" = lakeid,"Date" = icedata$Date,"Ice_Status" = icestatus,
+                             "Source" = icedata$Source,"Comments" = NA)
+
+    iceoutput = rbind(iceoutput,icedata)
+    }
   }
-  if (!is.null(iceoutput)){
-    iceoutput$Date=as.Date(iceoutput$Date)
-    iceoutput=iceoutput[order(iceoutput$Date),]
 
-    iceoutput=data.frame("Lake"=lakename,"LakeId"=lakeid,"Date"=iceoutput$Date,"Ice_Status"=iceoutput$Ice_Status,
-                         "Source"=iceoutput$Source,"Comments"=iceoutput$Comment)
+  if (nrow(iceoutput) > 0){
+    iceoutput = iceoutput[order(iceoutput$Date),]
   }else{
     warning("No ice data for this lake.")
   }
   }else{
-    iceoutput=print("No ice data for this lake")
+    iceoutput = print("No ice data for this lake")
   }
 
   # if (is.null(iceoutput)){
